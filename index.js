f8066cd0d2da7de84bc5cbb80f7fa6e94dabb5e8
pankyayadav <101920610+pankyayadav@users.noreply.github.com>
2024-06-29 21:01:14 +0000

    Change in Path..

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express()
 const port = 8081
 const bodyParser = require('body-parser')
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }))
 const nodemailer = require("nodemailer");
